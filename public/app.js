947a29a00f24f2a9a768073db7eb8193df2be0be
raimovasad <88774509+raimovasad@users.noreply.github.com>
2021-09-21 00:40:10 +0500

    blok3 end

--- public/app.js
+++ public/app.js
@@ -1,6 +1,47 @@
-document.querySelectorAll('.price').forEach(price =>{
+function toCurrency(price){
     price.textContent = new Intl.NumberFormat('en-EN',{
         currency: 'usd',
         style: 'currency'
-    }).format(price.textContent)
-})
+    }).format(price.innerText)
+}
+
+document.querySelectorAll('.price').forEach(c => toCurrency(c))
+
+const $card = document.querySelector('#card');
+if($card){
+    $card.addEventListener('click',(event)=>{
+        if(event.target.classList.contains('js-remove') ){
+            const id = event.target.dataset.id 
+            
+            fetch('/card/remove/'+id,{
+                method:'delete'
+            }).then(data=> data.json())
+            .then(card=>{
+               if(card.equips.length){
+                const html = card.equips.map(c=>{
+                    return `  <tr>
+                    <td style="font-family: Verdana; font-weight: 700;">${c.title}</td>
+                    <td style="font-weight: 600; font-size: 20px; text-align: center;">${c.count}</td>
+                    <td style="font-weight: 600; font-size: 20px;" class="price">${c.price}</td>
+                    <td>
+                        <button class="btn btn-small js-remove" data-id="${c.id}" type="submit">Cancel</button>
+                    </td>
+                </tr>`
+                }).join('')
+
+
+                $card.querySelector('#total-price').textContent = card.price
+                $card.querySelector('tbody').innerHTML = html
+                 $card.querySelectorAll('.price').forEach(c=> toCurrency(c))
+               }
+               else{
+                $card.innerHTML ='<p>Shoplist is empty!!!</p>'
+               }
+             
+
+            })
+
+        }
+
+    })
+}
--- routes/card.js
+++ routes/card.js
@@ -1,14 +1,32 @@
 const {Router} = require('express')
+const Card = require('../models/Card')
+const Equipment = require('../models/Equipment')
 
 const router = Router()
 
-router.get('/',(req,res)=>{
+router.get('/',async(req,res)=>{
+        const {equips,price} = await Card.fetch()
     
     res.render('card',{
         title: 'Shoplist',
+        equips,
+        price,
+        isCard: true
     })
 })
 
+router.post('/add',async(req,res)=>{
+    const equip = await Equipment.getById(req.body.id)
+    await Card.add(equip)
+    res.redirect('/card')
+})
+
+router.delete('/remove/:id',async(req,res)=>{
+    
+   const card = await Card.remove(req.params.id)
+    res.status(200).json(card)
+})
 
 
+ 
 module.exports = router
